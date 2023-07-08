a776d59aaf2591e82b6fc03dceebd077a8fe1d09
gs2 <georgiy.shevoroshkin@inteco.ch>
2023-07-08 17:45:28 +0200

    started implementing range css

--- src/app/components/general/filter/filter.component.ts
+++ src/app/components/general/filter/filter.component.ts
@@ -1,12 +1,26 @@
-import { ChangeDetectorRef, Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
-import { FilterCategoryEntity, FilterRangeEntity, FiltersEntity, SelectedFilterEntity, TagEntity } from 'src/app/services/filter/filter.model';
+import {
+  ChangeDetectorRef,
+  Component,
+  EventEmitter,
+  HostBinding,
+  Input,
+  OnInit,
+  Output,
+} from '@angular/core';
+import {
+  FilterCategoryEntity,
+  FilterRangeEntity,
+  FiltersEntity,
+  SelectedFilterEntity,
+  TagEntity,
+} from 'src/app/services/filter/filter.model';
 import { ImageComp } from '../../components.model';
 import { arrowIcon } from '../../components.constants';
 
 @Component({
   selector: 'app-filter',
   templateUrl: './filter.component.html',
-  styleUrls: ['./filter.component.scss']
+  styleUrls: ['./filter.component.scss'],
 })
 export class FilterComponent implements OnInit {
   @Input() filters!: FiltersEntity;
@@ -15,49 +29,81 @@ export class FilterComponent implements OnInit {
   selectedFilter: SelectedFilterEntity[] = [];
   arrowIcon: ImageComp = arrowIcon;
 
-  constructor(private cdref: ChangeDetectorRef) {
-  }
+  constructor(private cdref: ChangeDetectorRef) {}
 
   ngAfterContentChecked() {
     this.cdref.detectChanges();
   }
 
   ngOnInit() {
-    this.filters.ranges?.forEach(range => this.selectedFilter.push({ id: range.id, name: range.name, range: true, category: range.id, value: [range.values[0], range.values[range.values.length - 1]] }))
+    this.filters.ranges?.forEach((range) =>
+      this.selectedFilter.push({
+        id: range.id,
+        name: range.name,
+        range: true,
+        category: range.id,
+        value: [range.values[0], range.values[range.values.length - 1]],
+      })
+    );
   }
 
   toggleTag(category: FilterCategoryEntity, tag: TagEntity) {
     let changed = false;
-    this.selectedFilter.forEach(elem => {
+    this.selectedFilter.forEach((elem) => {
       if (elem.category == category.id) {
         if (elem.value.includes(tag.value)) {
           elem.value.splice(elem.value.indexOf(tag.value), 1);
           if (elem.value.length == 0) {
-            this.selectedFilter.splice(this.selectedFilter.findIndex(el => el.category == category.id), 1);
+            this.selectedFilter.splice(
+              this.selectedFilter.findIndex((el) => el.category == category.id),
+              1
+            );
           }
         } else {
-          elem.value.push(tag.value)
+          elem.value.push(tag.value);
         }
         changed = true;
       }
-    })
+    });
     if (!changed) {
-      this.selectedFilter.push({ id: category.id, name: category.name, range: false, category: category.id, value: [tag.value] })
+      this.selectedFilter.push({
+        id: category.id,
+        name: category.name,
+        range: false,
+        category: category.id,
+        value: [tag.value],
+      });
     }
-    this.filterEmitter.emit(this.selectedFilter.sort((a, b) => a.range == b.range ? 0 : a.range ? -1 : 1));
+    this.filterEmitter.emit(
+      this.selectedFilter.sort((a, b) =>
+        a.range == b.range ? 0 : a.range ? -1 : 1
+      )
+    );
   }
 
   changeRange(range: FilterRangeEntity, ref: HTMLInputElement) {
-    this.selectedFilter.filter(elem => elem.category == range.id)[0].value[ref.classList.contains("slider1") ? 0 : 1] = ref.value;
+    this.selectedFilter.filter((elem) => elem.category == range.id)[0].value[
+      ref.classList.contains('slider1') ? 0 : 1
+    ] = ref.value;
     ref.value = ref.value;
     this.filterEmitter.emit(this.selectedFilter);
   }
 
   reset() {
     this.selectedFilter = [];
-    this.filters.ranges?.forEach(range => this.selectedFilter.push({ id: range.id, name: range.name, category: range.id, range: true, value: [range.values[0], range.values[range.values.length - 1]] }))
+    this.filters.ranges?.forEach((range) =>
+      this.selectedFilter.push({
+        id: range.id,
+        name: range.name,
+        category: range.id,
+        range: true,
+        value: [range.values[0], range.values[range.values.length - 1]],
+      })
+    );
     this.filterEmitter.emit(this.selectedFilter);
-    document.querySelectorAll('.tag').forEach(elem => elem.classList.remove('active'));
+    document
+      .querySelectorAll('.tag')
+      .forEach((elem) => elem.classList.remove('active'));
   }
 
   /*@Input() filterValues!:{[key:string]:string};
@@ -77,5 +123,4 @@ export class FilterComponent implements OnInit {
   onFilterChange(){
     //elem.classList.add('selected');
   } */
-
 }
